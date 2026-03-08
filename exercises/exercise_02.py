14328764a492c74c4bfa3db4b25f09e6b75cb239
matheuspc3 <matheuspc3@hotmail.com>
2026-03-08 11:55:35 -0300

    exercicios de python

--- exercises/exercise_02.py
+++ exercises/exercise_02.py
@@ -2,3 +2,4 @@
 # correta e false caso o contrário. Logo após elabore um “mini-sistema” de checar a
 # senha inserida, onde o usuário tem 3 tentativas de senha e caso esse número seja
 # ultrapassado o programa é encerrado.
+    
